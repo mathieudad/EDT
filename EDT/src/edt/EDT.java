94c840f217ed44482a6c87b98e5e59d1af430a4c
mathieudad <43423223+mathieudad@users.noreply.github.com>
2020-05-25 15:49:43 +0200

    test 2

--- EDT/src/edt/EDT.java
+++ EDT/src/edt/EDT.java
@@ -16,7 +16,8 @@ public class EDT {
      */
     public static void main(String[] args) {
         // TODO code application logic 
-        System.out.print("hello");
+        System.out.print("bonjour");
+        
     }
     
 }
